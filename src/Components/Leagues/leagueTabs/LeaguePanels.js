bc8927f23413adf3963e8cca8b882920a67baab1
Michael Mangus <mjmangus16@yahoo.com>
2019-12-19 12:35:09 -0500

    debugging leaguePanels on heroku

--- src/Components/Leagues/leagueTabs/LeaguePanels.js
+++ src/Components/Leagues/leagueTabs/LeaguePanels.js
@@ -5,36 +5,37 @@ import SchedulePanel from "../LeagueSchedule/SchedulePanel";
 import RosterPanel from "../LeagueRoster/RosterPanel";
 import RoundsPanel from "../LeagueRounds/RoundsPanel";
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+// function TabPanel(props) {
+//   const { children, value, index, ...other } = props;
 
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      <Box p={3}>{children}</Box>
-    </Typography>
-  );
-}
+//   return (
+//     <Typography
+//       component="div"
+//       role="tabpanel"
+//       hidden={value !== index}
+//       id={`simple-tabpanel-${index}`}
+//       aria-labelledby={`simple-tab-${index}`}
+//       {...other}
+//     >
+//       <Box p={3}>{children}</Box>
+//     </Typography>
+//   );
+// }
 
 const LeaguePanels = ({ tabValue }) => {
   return (
-    <div style={{ marginBottom: 25 }}>
-      <TabPanel value={tabValue} index={0}>
-        <SchedulePanel />
-      </TabPanel>
-      <TabPanel value={tabValue} index={1}>
-        <RosterPanel />
-      </TabPanel>
-      <TabPanel value={tabValue} index={2}>
-        <RoundsPanel />
-      </TabPanel>
-    </div>
+    <div>HELLO</div>
+    // <div style={{ marginBottom: 25 }}>
+    //   <TabPanel value={tabValue} index={0}>
+    //     <SchedulePanel />
+    //   </TabPanel>
+    //   <TabPanel value={tabValue} index={1}>
+    //     <RosterPanel />
+    //   </TabPanel>
+    //   <TabPanel value={tabValue} index={2}>
+    //     <RoundsPanel />
+    //   </TabPanel>
+    // </div>
   );
 };
 
