fd2e9377a8c8aca415f4b3610989a04c86ce22bf
Michael Mangus <mjmangus16@yahoo.com>
2019-12-19 11:43:14 -0500

    fixed bug in axiosWithAuth in utils

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -10,7 +10,7 @@ export const axiosWithAuth = () => {
 
   if (currentUrl === "http://localhost:3000/") {
     baseURL = "http://localhost:5000/";
-  } else if (currentUrl === "https://bgp-fe-staging.herokuapp.com/") {
+  } else if (currentUrl === "https://dgl-fe-staging.herokuapp.com/") {
     baseURL = "https://dgl-be-staging.herokuapp.com/";
     // } else if (
     //   currentUrl === "http://www.baseballgamepredictor.com/" ||
